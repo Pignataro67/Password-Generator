321491d65002abc24e27a9efcb1af7e35bfd5b80
Pignataro67 <Pignataro67@gmail.com>
2021-10-23 07:33:09 -0700

    Adding JS-some var assign. and an obj.

--- script.js
+++ script.js
@@ -3,3 +3,13 @@ const lengthEl = document.getElementById('length')
 const uppercaseEl = document.getElementById('uppercase')
 const lowercaseEl = document.getElementById('lowercase')
 const numbersEl = document.getElementById('numbers')
+const symbolsEl = document.getElementById('symbols')
+const generateEl = document.getElementById('generate')
+const clipboardEl = document.getElementById('clipboard')
+
+const randomFunc = {
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  symbol: getRandomSymbol
+}
